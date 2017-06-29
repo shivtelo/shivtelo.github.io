305ae8757eaa78f7a361a011ff9ab12644e921d3
Shiv Kumar <shivtelo@gmail.com>
2017-06-30 03:55:47 +0530

    New Content
    Home Page

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/custom.css">
+	<title>Shiv Kumar</title>
+</head>
+<body>
+	<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+		<div class="container-fluid">
+		                <a class="navbar-brand" href="#">
+		                   	<!-- <img src="http://placehold.it/150x50/e74c3c/ffffff/?text=SHIV&textsize=15" alt="">-->
+							<img src="https://placeholdit.imgix.net/~text?txtsize=20&bg=e74c3c&txtclr=ffffff&txt=SHIV&w=150&h=50&txttrack=0" alt="">
+		                </a>
+		            
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+									<ul class="nav navbar-nav">
+										<li><a href="#home">Home</a></li>
+										<li><a href="#aboutme">About Me</a></li>
+										<li><a href="#hobbies">Hobbies</a></li>
+										<li><a href="#skills">Skills</a></li>
+										<li><a href="resume.pdf">Resume</a></li>
+										<li><a href="#contactme">Contact Me</a></li>
+									</ul>
+						</div>
+		</div>
+	</nav>
+	<div class="uparseniche" align="center">
+	
+	<div id="home">
+			<div  class="bahar anima">
+			<span class="lft">
+			Welcome<br>
+			I, Shiv Kumar, am a 3rd year Undergraduate Student at Department of Computer Science and Engineering in Maulana Azad National Institute of Technology, Bhopal.
+	<br>
+			</span>
+			<span class="rht">
+			<img src="images/3.jpg" class="midimg">
+			</span>
+			</div>
+	</div>
+	
+
+<div id="aboutme">
+<div class="bahar anima">
+
+<span class="lft">
+	About Me<br>
+	
+	I was born and brought up in Bokaro Steel City, Jharkhand.
+	I also completed my Xth and Intermediate here.
+	<br>
+	<br>
+	You'll find me as:
+	A person passionate about competitive coding. An enthusiastic learner. A friend eager to help.
+	</span>
+
+<!--<span class="rht">
+<img src="images/4.jpg" class="midimg">
+</span>-->
+</div>
+</div>
+<div id="hobbies">
+<div class="bahar anima">
+
+<span class="lft">
+	Hobbies<br>
+	<br>
+	In free time,I love reading novels(especially Agatha Christie), watching movies or anime.
+	I like playing football, chess.
+	
+	<br><br>
+	My favourite quote: Enjoy every moment of life until the day you are dead.
+	</span>
+<span class="rht">
+<img src="images/4.jpg" class="midimg">
+</span>
+</div>
+</div>
+<div class="brakes"></div>
+<div id="skills" class="bahar">
+<span class="lft">
+	Skills<br>
+	<br>
+	=>Has worked with: C,C++,Python,Java SE,PHP
+	<br>=><a href="#">Link </a>to my software engineering project.
+	<br><br> 
+	=>Worked in my college's webclub as a backend developer.
+	<br>
+	=>Currently taking stanford machine learning program on coursera completes 6th august.
+	</span>
+	<!--<span class="rht">
+	<img src="images/2.jpg" class="midimg"></span>-->
+</div>
+<div class="brakes"></div>
+<div id="contactme" class="bahar anima">
+<span class="lft">
+	Contact Me<br>
+	<div style="float: right;">
+		
+	<a href="shivtelo@gmail.com">gmail</a>
+		<br>
+		<a href="facbook.com/shivtelo">facbook</a>
+		facebook<br>
+		<a href="linkedin.com/shivtelo">linkedin</a>
+		linkedin<br>
+		<a href="github.com/shivtelo">github</a>
+		github<br>
+		</div>
+	</span>
+
+<span class="rht">
+<img src="images/2.jpg" class="midimg">
+</span>
+	<!--<img src="http://mfp.comeze.com/images/avatar.jpg">-->
+</div>
+</div>
+</body>
+</html>
